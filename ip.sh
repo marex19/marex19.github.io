8b7761a8b081a793b5d23070c40ada11efbd84cb
Marek Franke <mfranke@ct01.(none)>
2019-01-13 15:26:40 +0000

    IP Updated

---
+++ ip.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+log="/var/log/ip/ip.log"
+tmp="/tmp/tmp_ip.html"
+ip=`tail -1 $log`
+new_ip=`curl -4 -s http://icanhazip.com`
+
+#echo $ip
+#echo $new_ip
+
+if [[ $ip != $new_ip ]]; then
+  echo $new_ip >> $log
+  cd /home/mfranke/enter/
+  cat enter.html | sed "s/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/$new_ip/g" > enter_new.html
+  mv enter_new.html enter.html
+  git commit -a -m "IP Updated" > /dev/null 2>&1
+  git push -u origin master > /dev/null 2>&1
+fi
+
+
