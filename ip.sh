1880a64f010d31da626fe6bb3084614e2919ab27
Marek Franke <mfranke@ct01.marex.local>
2019-02-03 04:00:02 +0000

    IP Updated

--- ip.sh
+++ ip.sh
@@ -9,18 +9,23 @@ new_ip=`curl -4 -s http://icanhazip.com`
 
 if [[ $ip != $new_ip ]]; then
   echo $new_ip >> $log
-  cd /home/mfranke/enter/
-  cat enter.html | sed "s/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/$new_ip/g" > enter_new.html
-  mv enter_new.html enter.html
+  cd /home/mfranke/marex19.github.io/
+  cat index.html | sed "s/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/$new_ip/g" > index_new.html
+  cp index_new.html index.html
   git commit -a -m "IP Updated" > /dev/null 2>&1
   git push -u origin master > /dev/null 2>&1
   # UPDATE RANDOM BACKGROUND IMAGE
   #i=`find /var/www/mediadb/preview/ -name "*.jpg" | sort -R | tail -1`
   #echo $i
   #cp $i /var/www/pics/background.jpg
+#fi
+  export PATH=$PATH:/opt/postgresql/latest/bin
+  image=`psql -h localhost -U mediadb mediadb < /home/mfranke/marex19.github.io/hiking.sql | sort -R | tail -1 | sed 's/^\ //'`
+  title=`echo $image | awk 'BEGIN {FS="/"} {print $(NF-1)}'`
+  font=`convert -list font | grep "Font" | sed 's/^\ \ Font:\ //' | sort -R | tail -1`
+  #echo "$title"
+  #echo $font
+  #convert -resize 50% "$image" /var/www/pics/tmp_background.jpg
+  convert -resize 50% -font $font -fill black -pointsize 20 -annotate +50+100 "$title" "$image" /var/www/pics/background.jpg
 fi
-
-export PATH=$PATH:/opt/postgresql/latest/bin
-image=`psql -h localhost -U mediadb mediadb < /home/mfranke/marex19.github.io/hiking.sql | sort -R | tail -1 | sed 's/^\ //'`
-convert -resize 50% "$image" /var/www/pics/background.jpg
 
