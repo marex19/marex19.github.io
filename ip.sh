b8a803490dcb1087d1d86379f7da90e5db53c067
marex19 <mfranke78@yahoo.de>
2019-02-10 11:32:22 +0100

    IP Updated

--- ip.sh
+++ ip.sh
@@ -12,20 +12,32 @@ if [[ $ip != $new_ip ]]; then
   cd /home/mfranke/marex19.github.io/
   cat index.html | sed "s/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/$new_ip/g" > index_new.html
   cp index_new.html index.html
-  git commit -a -m "IP Updated" > /dev/null 2>&1
-  git push -u origin master > /dev/null 2>&1
+  #git commit -a -m "IP Updated" > /dev/null 2>&1
+  #git push -u origin master > /dev/null 2>&1
   # UPDATE RANDOM BACKGROUND IMAGE
   #i=`find /var/www/mediadb/preview/ -name "*.jpg" | sort -R | tail -1`
   #echo $i
   #cp $i /var/www/pics/background.jpg
-#fi
+fi
   export PATH=$PATH:/opt/postgresql/latest/bin
   image=`psql -h localhost -U mediadb mediadb < /home/mfranke/marex19.github.io/hiking.sql | sort -R | tail -1 | sed 's/^\ //'`
   title=`echo $image | awk 'BEGIN {FS="/"} {print $(NF-1)}'`
   font=`convert -list font | grep "Font" | sed 's/^\ \ Font:\ //' | sort -R | tail -1`
   #echo "$title"
   #echo $font
-  #convert -resize 50% "$image" /var/www/pics/tmp_background.jpg
-  convert -resize 50% -font $font -fill black -pointsize 20 -annotate +50+100 "$title" "$image" /var/www/pics/background.jpg
-fi
+  cp "$image" /tmp/tmp.jpg
+  convert -resize 50% -font $font -fill black -pointsize 20 -annotate +50+100 "$title" /tmp/tmp.jpg /var/www/html/pics/background.jpg
+  rm -f /tmp/tmp.jpg
+#fi
+
+
+
+
+
+
+
+
+
+
+
 
